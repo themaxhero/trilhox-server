2dfd0ec05da1487e4b9a6e4a17953ff539e1f7ed
Marcelo A. de L. Santos <contato.maxhero@gmail.com>
2019-04-10 14:26:59 -0300

    create input types on Typescript-side

---
+++ src/input.types.ts
@@ -0,0 +1,66 @@
+import { Permission } from "./entity/Member";
+
+export interface IAddBookToKanbanInput{
+    name: string;
+}
+
+export interface IAddLabelToKanbanInput{
+    name: string;
+    color: string;
+}
+
+export interface IAddMemberToKanbanInput{
+    user: string;
+    permission: Permission;
+}
+
+export interface IAddCardToBookInput{
+    name?: string;
+}
+
+export interface IAddTaskToCardInput{
+    name: string;
+    active?: boolean;
+}
+
+export interface IPostCommentOnCardInput{
+    author: string;
+    content: string;
+}
+
+export interface IUpdateUserInput{
+    email?: string;
+    name?: string;
+    avatar?: string;
+}
+
+export interface IUpdateKanbanInput{
+    name?: string;
+}
+
+export interface IUpdateMemberInput{
+    permission: Permission;
+}
+
+export interface IUpdateBookInput{
+    name: string;
+}
+
+export interface IUpdateCardInput{
+    name?: string;
+    description?: string;
+}
+
+export interface IUpdateLabelInput{
+    name?: string;
+    color?: string;
+}
+
+export interface IUpdateTaskInput{
+    name?: string;
+    active?: boolean;
+}
+
+export interface IUpdateCommentInput{
+    content: string;
+}
